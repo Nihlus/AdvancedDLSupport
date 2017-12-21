c74e2bb0e1006a850db946221fd54ebc4d80db90
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-12-21 22:03:08 +0100

    Break path resolvers out into their own classes.

---
+++ AdvancedDLSupport/PathResolvers/MacOSPathResolver.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace AdvancedDLSupport
+{
+    /// <summary>
+    /// Resolves library paths on macOS.
+    /// </summary>
+    public class MacOSPathResolver : ILibraryPathResolver
+    {
+        /// <inheritdoc />
+        public string Resolve(string library)
+        {
+            var libraryPaths = Environment.GetEnvironmentVariable("DYLD_FRAMEWORK_PATH").Split(':');
+
+            string libraryLocation;
+            foreach (var path in libraryPaths)
+            {
+                libraryLocation = Path.GetFullPath(Path.Combine(path, library));
+                if (File.Exists(libraryLocation))
+                {
+                    return libraryLocation;
+                }
+            }
+
+            libraryPaths = Environment.GetEnvironmentVariable("DYLD_LIBRARY_PATH").Split(':');
+            foreach (var path in libraryPaths)
+            {
+                libraryLocation = Path.GetFullPath(Path.Combine(path, library));
+                if (File.Exists(libraryLocation))
+                {
+                    return libraryLocation;
+                }
+            }
+
+            libraryPaths = Environment.GetEnvironmentVariable("DYLD_FALLBACK_FRAMEWORK_PATH").Split(':');
+            foreach (var path in libraryPaths)
+            {
+                libraryLocation = Path.GetFullPath(Path.Combine(path, library));
+                if (File.Exists(libraryLocation))
+                {
+                    return libraryLocation;
+                }
+            }
+
+            libraryPaths = Environment.GetEnvironmentVariable("DYLD_FALLBACK_LIBRARY_PATH").Split(':');
+            foreach (var path in libraryPaths)
+            {
+                libraryLocation = Path.GetFullPath(Path.Combine(path, library));
+                if (File.Exists(libraryLocation))
+                {
+                    return libraryLocation;
+                }
+            }
+
+            throw new FileNotFoundException("The specified library was not found in any of the loader search paths.");
+        }
+    }
+}
