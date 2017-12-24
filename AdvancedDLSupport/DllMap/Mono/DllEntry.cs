db0ac80e2f77cf6c7f46f65e7faec96d0097eea1
Jarl Gullberg <jarl.gullberg@gmail.com>
2017-12-24 12:30:56 +0100

    Add the DllEntry class, and a number of abbreviations.

---
+++ AdvancedDLSupport/DllMap/Mono/DllEntry.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+
+namespace AdvancedDLSupport.DllMap.Mono
+{
+    /// <summary>
+    /// Represents a subentry in a Mono DllMap for specific functions.
+    /// </summary>
+    [XmlRoot("dllentry")]
+    internal class DllEntry : MappingBase
+    {
+        /// <summary>
+        /// Gets or sets the target library that the entry should map to.
+        /// </summary>
+        [XmlAttribute("dll")]
+        public string TargetLibrary { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the source symbol that the entry maps.
+        /// </summary>
+        [XmlAttribute("name")]
+        public string SourceSymbol { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the target symbol that the entry should map to.
+        /// </summary>
+        [XmlAttribute("target")]
+        public string TargetSymbol { get; set; }
+    }
+}
