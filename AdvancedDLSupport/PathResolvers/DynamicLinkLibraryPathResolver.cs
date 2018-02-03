d4b0c6cb6fb8cfd2587dd618e35b190d5c4ee255
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-02-03 12:56:32 +0100

    Update the DynamicLinkLibraryPathResolver class.
    * Make it nonstatic
    * Implement ILibraryPathResolver
    * Split the local path resolution out into its own resolver
    * Improve direct absolute path scanning
    * Move the Mono internal extension here

--- AdvancedDLSupport/PathResolvers/DynamicLinkLibraryPathResolver.cs
+++ AdvancedDLSupport/PathResolvers/DynamicLinkLibraryPathResolver.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using AdvancedDLSupport.Extensions;
 using JetBrains.Annotations;
 
 namespace AdvancedDLSupport
@@ -11,14 +12,29 @@ namespace AdvancedDLSupport
     /// <summary>
     /// Resolves dynamic link library paths.
     /// </summary>
-    internal static class DynamicLinkLibraryPathResolver
+    internal class DynamicLinkLibraryPathResolver : ILibraryPathResolver
     {
+        [NotNull]
+        private static readonly ILibraryPathResolver LocalPathResolver;
+
         [NotNull]
         private static readonly ILibraryPathResolver PathResolver;
 
+        private bool SearchLocalFirst { get; }
+
         static DynamicLinkLibraryPathResolver()
         {
+            LocalPathResolver = new LocalPathResolver();
             PathResolver = SelectPathResolver();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DynamicLinkLibraryPathResolver"/> class.
+        /// </summary>
+        /// <param name="searchLocalFirst">Whether or not the local search pattern should be followed first.</param>
+        public DynamicLinkLibraryPathResolver(bool searchLocalFirst = true)
+        {
+            SearchLocalFirst = searchLocalFirst;
         }
 
         [NotNull]
@@ -47,6 +63,12 @@ namespace AdvancedDLSupport
             throw new PlatformNotSupportedException($"Cannot resolve linker paths on this platform: {RuntimeInformation.OSDescription}");
         }
 
+        /// <inheritdoc />
+        public ResolvePathResult Resolve(string library)
+        {
+            return ResolveAbsolutePath(library, SearchLocalFirst);
+        }
+
         /// <summary>
         /// Resolves the absolute path to the given library.
         /// </summary>
@@ -57,46 +79,67 @@ namespace AdvancedDLSupport
         /// Thrown if the current platform doesn't have a path
         /// resolver defined.</exception>
         /// <exception cref="FileNotFoundException">Thrown if no library file can be found.</exception>
-        public static ResolvePathResult ResolveAbsolutePath([NotNull] string library, bool localFirst)
+        private ResolvePathResult ResolveAbsolutePath([NotNull] string library, bool localFirst)
         {
             var candidates = GenerateLibraryCandidates(library).ToList();
+
+            if (library.IsValidPath())
+            {
+                foreach (var candidate in candidates)
+                {
+                    if (File.Exists(candidate))
+                    {
+                        return ResolvePathResult.FromSuccess(Path.GetFullPath(candidate));
+                    }
+                }
+            }
 
             if (localFirst)
             {
                 foreach (var candidate in candidates)
                 {
-                    var executingDir = Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName;
-                    var libraryLocation = Path.GetFullPath(Path.Combine(executingDir, candidate));
-                    if (File.Exists(libraryLocation))
+                    var result = LocalPathResolver.Resolve(candidate);
+                    if (result.IsSuccess)
                     {
-                        return ResolvePathResult.FromSuccess(libraryLocation);
+                        return result;
                     }
                 }
             }
 
             foreach (var candidate in candidates)
             {
-                if (Path.IsPathRooted(candidate) && File.Exists(candidate))
+                var result = PathResolver.Resolve(candidate);
+                if (result.IsSuccess)
                 {
-                    return ResolvePathResult.FromSuccess(Path.GetFullPath(candidate));
+                    return result;
                 }
+            }
 
-                try
-                {
-                    var result = PathResolver.Resolve(candidate);
-                    return ResolvePathResult.FromSuccess(result);
-                }
-                catch (FileNotFoundException)
-                {
-                }
+            if (!(Type.GetType("Mono.Runtime") is null) && library == "__Internal")
+            {
+                // Mono extension: Search the main program
+                return ResolvePathResult.FromSuccess(null);
             }
 
             return ResolvePathResult.FromError(new FileNotFoundException("The specified library was not found in any of the loader search paths.", library));
         }
 
+        /// <summary>
+        /// Generates a set of platform-specific candidate library names.
+        /// </summary>
+        /// <param name="library">The library name to generate candidates for.</param>
+        /// <returns>A list of candidates.</returns>
         [NotNull, ItemNotNull]
         private static IEnumerable<string> GenerateLibraryCandidates([NotNull] string library)
         {
+            bool doesLibraryContainPath = false;
+            var parentDirectory = Path.GetDirectoryName(library);
+            if (library.IsValidPath())
+            {
+                library = Path.GetFileName(library);
+                doesLibraryContainPath = true;
+            }
+
             var candidates = new List<string>();
             candidates.Add(library);
 
@@ -127,6 +170,12 @@ namespace AdvancedDLSupport
                 {
                     candidates.Add($"{prefix}{library}{suffix}");
                 }
+            }
+
+            // If we have a parent path we're looking at, mutate the candidate list to include the parent path
+            if (doesLibraryContainPath)
+            {
+                candidates = candidates.Select(c => Path.Combine(parentDirectory, c)).ToList();
             }
 
             return candidates;
---
+++ AdvancedDLSupport/PathResolvers/LocalPathResolver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace AdvancedDLSupport
+{
+    /// <summary>
+    /// Resolves locally bundled library paths.
+    /// </summary>
+    internal class LocalPathResolver : ILibraryPathResolver
+    {
+        private readonly string _executingDirectory;
+        private readonly string _libraryDirectory;
+        private readonly string _platformLibraryDirectory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalPathResolver"/> class.
+        /// </summary>
+        public LocalPathResolver()
+        {
+            _executingDirectory = Directory.GetParent(Assembly.GetExecutingAssembly().Location).FullName;
+            _libraryDirectory = Path.Combine(_executingDirectory, "lib");
+            _platformLibraryDirectory = Path.Combine(_libraryDirectory, Environment.Is64BitProcess ? "x64" : "x86");
+        }
+
+        /// <inheritdoc />
+        public ResolvePathResult Resolve(string library)
+        {
+            // First, check next to the executable
+            var libraryLocation = Path.GetFullPath(Path.Combine(_executingDirectory, library));
+            if (File.Exists(libraryLocation))
+            {
+                return ResolvePathResult.FromSuccess(libraryLocation);
+            }
+
+            // Check the local library directory
+            libraryLocation = Path.GetFullPath(Path.Combine(_libraryDirectory, library));
+            if (File.Exists(libraryLocation))
+            {
+                return ResolvePathResult.FromSuccess(libraryLocation);
+            }
+
+            // Check platform-specific directory
+            libraryLocation = Path.GetFullPath(Path.Combine(_platformLibraryDirectory, library));
+            if (File.Exists(libraryLocation))
+            {
+                return ResolvePathResult.FromSuccess(libraryLocation);
+            }
+
+            return ResolvePathResult.FromError(new FileNotFoundException("No local copy of the given library could be found.", library));
+        }
+    }
+}
