5536a3970a3f67d2d5d228b02c2ac9e35372e561
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-02-04 19:22:01 +0100

    Clean up unused usings.

--- AdvancedDLSupport/PathResolvers/DynamicLinkLibraryPathResolver.cs
+++ AdvancedDLSupport/PathResolvers/DynamicLinkLibraryPathResolver.cs
@@ -21,7 +21,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using AdvancedDLSupport.Extensions;
 using JetBrains.Annotations;
