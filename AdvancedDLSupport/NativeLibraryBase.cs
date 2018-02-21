8720c64df5a654f9a2f6f4d73a54a9582649bdc8
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-02-21 12:58:23 +0100

    Suppress the finalizer in class with both dispose and finalizer.

--- AdvancedDLSupport/NativeLibraryBase.cs
+++ AdvancedDLSupport/NativeLibraryBase.cs
@@ -132,6 +132,8 @@ namespace AdvancedDLSupport
                 return;
             }
 
+            GC.SuppressFinalize(this);
+
             IsDisposed = true;
 
             PlatformLoader.CloseLibrary(_libraryHandle);
