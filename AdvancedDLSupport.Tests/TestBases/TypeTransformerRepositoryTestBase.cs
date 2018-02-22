b489448e0c68a05deee4ff2268c853d0a29c07b1
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-02-22 13:55:41 +0100

    Add tests for the type transformer repository.

---
+++ AdvancedDLSupport.Tests/TestBases/TypeTransformerRepositoryTestBase.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+
+namespace AdvancedDLSupport.Tests.TestBases
+{
+    public class TypeTransformerRepositoryTestBase
+    {
+        [NotNull]
+        protected TypeTransformerRepository Repository { get; }
+
+        protected TypeTransformerRepositoryTestBase()
+        {
+            Repository = new TypeTransformerRepository();
+        }
+    }
+}
