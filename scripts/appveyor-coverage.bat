45a7d5baf12e78ea83fe8f6f3e3c27779f3b6b5a
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-06 17:11:00 +0100

    Break up long lines in build scripts.

--- scripts/appveyor-coverage.bat
+++ scripts/appveyor-coverage.bat
@@ -1,7 +1,30 @@
+@echo off
+
+:: Install AltCover
 nuget install altcover -OutputDirectory altcover -Version 1.6.230
-dotnet run --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --configuration %CONFIGURATION% -- -i=AdvancedDLSupport.Tests\bin\%CONFIGURATION%\netcoreapp2.0 -o=instrumented-adl -x=coverage-adl.xml --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+ --assemblyExcludeFilter=Mono\.DllMap.+
-dotnet run --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --configuration %CONFIGURATION% -- -i=Mono.DllMap.Tests\bin\%CONFIGURATION%\netcoreapp2.0 -o=instrumented-mdl -x=coverage-mdl.xml --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+
+
+:: Instrument the test assemblies
+dotnet run^
+ --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --configuration %CONFIGURATION% --^
+ -i=AdvancedDLSupport.Tests\bin\%CONFIGURATION%\netcoreapp2.0 -o=instrumented-adl -x=coverage-adl.xml^
+ --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+ --assemblyExcludeFilter=Mono\.DllMap.+
+
+dotnet run^
+ --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --configuration %CONFIGURATION% --^
+ -i=Mono.DllMap.Tests\bin\%CONFIGURATION%\netcoreapp2.0 -o=instrumented-mdl -x=coverage-mdl.xml^
+ --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+
+
+:: Copy them to their build directories
 copy /y instrumented-adl\* AdvancedDLSupport.Tests\bin\%CONFIGURATION%\netcoreapp2.0
 copy /y instrumented-mdl\* Mono.DllMap.Tests\bin\%CONFIGURATION%\netcoreapp2.0
-dotnet run --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --no-build -- runner -x "dotnet" -r "AdvancedDLSupport.Tests\bin\%CONFIGURATION%\netcoreapp2.0" -- test AdvancedDLSupport.Tests --no-build
-dotnet run --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --no-build -- runner -x "dotnet" -r "Mono.DllMap.Tests\bin\%CONFIGURATION%\netcoreapp2.0" -- test Mono.DllMap.Tests --no-build
+
+:: And run coverage
+dotnet run^
+ --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj^ --no-build --^
+ runner -x "dotnet" -r "AdvancedDLSupport.Tests\bin\%CONFIGURATION%\netcoreapp2.0" --^
+ test AdvancedDLSupport.Tests --no-build
+
+dotnet run^
+ --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --no-build --^
+ runner -x "dotnet" -r "Mono.DllMap.Tests\bin\%CONFIGURATION%\netcoreapp2.0" --^
+ test Mono.DllMap.Tests --no-build
--- scripts/travis-coverage.sh
+++ scripts/travis-coverage.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+# Install AltCover
 nuget install altcover -OutputDirectory altcover -Version 1.6.230
-dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration Debug -- -i=AdvancedDLSupport.Tests/bin/Debug/netcoreapp2.0 -o=instrumented-adl -x=coverage-adl.xml --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+ --assemblyExcludeFilter=Mono\.DllMap.+
-dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration Debug -- -i=Mono.DllMap.Tests/bin/Debug/netcoreapp2.0 -o=instrumented-mdl -x=coverage-mdl.xml --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+
+
+# Instrument the test assemblies
+dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration Debug --\
+ -i=AdvancedDLSupport.Tests/bin/Debug/netcoreapp2.0 -o=instrumented-adl -x=coverage-adl.xml\
+ --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+ --assemblyExcludeFilter=Mono\.DllMap.+
+
+dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration Debug --\
+ -i=Mono.DllMap.Tests/bin/Debug/netcoreapp2.0 -o=instrumented-mdl -x=coverage-mdl.xml\
+ --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+
+
+# Copy them to their build directories
 cp instrumented-adl/* AdvancedDLSupport.Tests/bin/Debug/netcoreapp2.0
 cp instrumented-mdl/* Mono.DllMap.Tests/bin/Debug/netcoreapp2.0
-dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --no-build -- runner -x "dotnet" -r "AdvancedDLSupport.Tests/bin/Debug/netcoreapp2.0" -- test AdvancedDLSupport.Tests --no-build
-dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --no-build -- runner -x "dotnet" -r "Mono.DllMap.Tests/bin/Debug/netcoreapp2.0" -- test Mono.DllMap.Tests --no-build
+
+# And run coverage
+dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --no-build --\
+ runner -x "dotnet" -r "AdvancedDLSupport.Tests/bin/Debug/netcoreapp2.0" --\
+ test AdvancedDLSupport.Tests --no-build
+
+dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --no-build --\
+ runner -x "dotnet" -r "Mono.DllMap.Tests/bin/Debug/netcoreapp2.0" --\
+ test Mono.DllMap.Tests --no-build
