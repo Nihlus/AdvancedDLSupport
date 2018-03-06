efbf4dccd98d8174deac0896cf98d73bdb80da02
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-06 16:58:34 +0100

    Make scripts executable before install.

---
+++ scripts/appveyor-coverage.bat
@@ -0,0 +1,7 @@
+nuget install altcover -OutputDirectory altcover -Version 1.6.230
+dotnet run --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --configuration %CONFIGURATION% -- -i=AdvancedDLSupport.Tests\bin\%CONFIGURATION%\netcoreapp2.0 -o=instrumented-adl -x=coverage-adl.xml --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+ --assemblyExcludeFilter=Mono\.DllMap.+
+dotnet run --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --configuration %CONFIGURATION% -- -i=Mono.DllMap.Tests\bin\%CONFIGURATION%\netcoreapp2.0 -o=instrumented-mdl -x=coverage-mdl.xml --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+
+copy /y instrumented-adl\* AdvancedDLSupport.Tests\bin\%CONFIGURATION%\netcoreapp2.0
+copy /y instrumented-mdl\* Mono.DllMap.Tests\bin\%CONFIGURATION%\netcoreapp2.0
+dotnet run --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --no-build -- runner -x "dotnet" -r "AdvancedDLSupport.Tests\bin\%CONFIGURATION%\netcoreapp2.0" -- test AdvancedDLSupport.Tests --no-build
+dotnet run --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --no-build -- runner -x "dotnet" -r "Mono.DllMap.Tests\bin\%CONFIGURATION%\netcoreapp2.0" -- test Mono.DllMap.Tests --no-build
