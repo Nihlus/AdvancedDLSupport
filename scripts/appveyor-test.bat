ef03e46cd5783ddfaa098349be5f5ea990168956
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-08 23:46:07 +0100

    Cache, remove, test, then reinstate platform variable.

--- scripts/appveyor-test.bat
+++ scripts/appveyor-test.bat
@@ -1,4 +1,9 @@
 @echo off
 
+set CACHED_PLATFORM=%PLATFORM%
+set PLATFORM=
+
 dotnet test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
 dotnet test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
+
+set PLATFORM=%CACHED_PLATFORM%
