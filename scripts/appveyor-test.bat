3c5e97a58dc2f7f4851f2c529ff5a3a70e2bfd79
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-09 19:03:28 +0100

    And for normal tests.

--- scripts/appveyor-test.bat
+++ scripts/appveyor-test.bat
@@ -5,8 +5,14 @@ if "%PLATFORM"=="Any CPU" (
 	set PLATFORM=
 )
 
-dotnet test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
-dotnet test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
+:: Determine the dotnet runtime to use
+set DOTNET_EXE="C:\Program Files\dotnet\dotnet.exe"
+if "%PLATFORM%"=="x86" (
+	set DOTNET_EXE="C:\Program Files (x86)\dotnet\dotnet.exe"
+)
+
+"%DOTNET_EXE%" test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
+"%DOTNET_EXE%" test --configuration %CONFIGURATION% --no-build Mono.DllMap.Tests
 
 if "%PLATFORM"=="Any CPU" (
 	set PLATFORM=%CACHED_PLATFORM%
