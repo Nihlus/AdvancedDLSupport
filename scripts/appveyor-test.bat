cc1634057807b79748a42a7d1647373686b9fd16
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-09 19:03:53 +0100

    Correct order.

--- scripts/appveyor-test.bat
+++ scripts/appveyor-test.bat
@@ -1,14 +1,14 @@
 @echo off
 
-if "%PLATFORM"=="Any CPU" (
-	set CACHED_PLATFORM=%PLATFORM%
-	set PLATFORM=
-)
-
 :: Determine the dotnet runtime to use
 set DOTNET_EXE="C:\Program Files\dotnet\dotnet.exe"
 if "%PLATFORM%"=="x86" (
 	set DOTNET_EXE="C:\Program Files (x86)\dotnet\dotnet.exe"
+)
+
+if "%PLATFORM"=="Any CPU" (
+	set CACHED_PLATFORM=%PLATFORM%
+	set PLATFORM=
 )
 
 "%DOTNET_EXE%" test --configuration %CONFIGURATION% --no-build AdvancedDLSupport.Tests
