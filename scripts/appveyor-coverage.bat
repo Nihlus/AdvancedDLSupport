bd66324b9dac9d8af0f462c44e53b0d93dde881c
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-11 13:58:16 +0100

    Move caching, and do another internal platform caching.

--- scripts/appveyor-coverage.bat
+++ scripts/appveyor-coverage.bat
@@ -34,16 +34,16 @@ nuget install altcover -OutputDirectory altcover -Version 1.6.230
 copy /y instrumented-adl\* AdvancedDLSupport.Tests\bin\%OUTPUT_DIR%\netcoreapp2.0
 copy /y instrumented-mdl\* Mono.DllMap.Tests\bin\%OUTPUT_DIR%\netcoreapp2.0
 
-:: Restore platform
-set "PLATFORM=%CACHED_PLATFORM%"
-
 :: And run coverage
 "%DOTNET_EXE%" run^
  --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --no-build --configuration %CONFIGURATION% --^
  runner -x "./scripts/dotnet-wrapper.bat" -r "AdvancedDLSupport.Tests\bin\%OUTPUT_DIR%\netcoreapp2.0" --^
- test AdvancedDLSupport.Tests --no-build
+ %CACHED_PLATFORM% test AdvancedDLSupport.Tests --no-build
 
 "%DOTNET_EXE%" run^
  --project altcover\altcover.1.6.230\tools\netcoreapp2.0\AltCover\altcover.core.fsproj --no-build --configuration %CONFIGURATION% --^
  runner -x "./scripts/dotnet-wrapper.bat" -r "Mono.DllMap.Tests\bin\%OUTPUT_DIR%\netcoreapp2.0" --^
- test Mono.DllMap.Tests --no-build
+ %CACHED_PLATFORM% test Mono.DllMap.Tests --no-build
+
+:: Restore platform
+set "PLATFORM=%CACHED_PLATFORM%"
--- scripts/dotnet-wrapper.bat
+++ scripts/dotnet-wrapper.bat
@@ -1,10 +1,17 @@
 @echo off
 
+:: Extract all but the first of the arguments
+for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
+
 :: Determine the dotnet runtime to use
 set "DOTNET_EXE=C:\Program Files\dotnet\dotnet.exe"
 if "%PLATFORM%"=="x86" (
 	set "DOTNET_EXE=C:\Program Files (x86)\dotnet\dotnet.exe"
 )
+
+:: Cache the current platform variable, and set it to the passed-in variable
+set "INTERNAL_CACHED_PLATFORM=%PLATFORM%"
+set "PLATFORM=%1"
 
 :: Clear the platform if it's Any CPU
 if "%PLATFORM"=="Any CPU" (
@@ -12,8 +19,11 @@ if "%PLATFORM"=="Any CPU" (
 	set PLATFORM=
 )
 
-"%DOTNET_EXE%" %*
-"%DOTNET_EXE%" %*
+"%DOTNET_EXE%" %ALL_BUT_FIRST%
+"%DOTNET_EXE%" %ALL_BUT_FIRST%
+
+:: Restore the cached variable
+set "PLATFORM=%INTERNAL_CACHED_PLATFORM%"
 
 if "%PLATFORM"=="Any CPU" (
 	set "PLATFORM=%CACHED_PLATFORM%"
