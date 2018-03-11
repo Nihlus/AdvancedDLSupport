fb98808b2cb534d0776e0ab0649c74563bec3e11
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-11 23:40:30 +0100

    Use updated altcover version on travis.

--- scripts/travis-coverage.sh
+++ scripts/travis-coverage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Install AltCover
-nuget install altcover -OutputDirectory altcover -Version 1.6.230
+nuget install altcover -OutputDirectory altcover -Version 2.0.324
 
 # Instrument the test assemblies
-dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${BuildConfiguration} --\
+dotnet run --project altcover/altcover.2.0.324/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${BuildConfiguration} --\
  -i=AdvancedDLSupport.Tests/bin/${BuildConfiguration}/netcoreapp2.0 -o=instrumented-adl -x=coverage-adl.xml --opencover\
  --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+ --assemblyExcludeFilter=Mono\.DllMap.+
 
-dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${BuildConfiguration} --\
+dotnet run --project altcover/altcover.2.0.324/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${BuildConfiguration} --\
  -i=Mono.DllMap.Tests/bin/${BuildConfiguration}/netcoreapp2.0 -o=instrumented-mdl -x=coverage-mdl.xml --opencover\
  --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+
 
@@ -17,10 +17,10 @@ cp instrumented-adl/* AdvancedDLSupport.Tests/bin/${BuildConfiguration}/netcorea
 cp instrumented-mdl/* Mono.DllMap.Tests/bin/${BuildConfiguration}/netcoreapp2.0
 
 # And run coverage
-dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${BuildConfiguration} --no-build --\
+dotnet run --project altcover/altcover.2.0.324/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${BuildConfiguration} --no-build --\
  runner -x "dotnet" -r "AdvancedDLSupport.Tests/bin/${BuildConfiguration}/netcoreapp2.0" --\
  test AdvancedDLSupport.Tests --no-build
 
-dotnet run --project altcover/altcover.1.6.230/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${BuildConfiguration} --no-build --\
+dotnet run --project altcover/altcover.2.0.324/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${BuildConfiguration} --no-build --\
  runner -x "dotnet" -r "Mono.DllMap.Tests/bin/${BuildConfiguration}/netcoreapp2.0" --\
  test Mono.DllMap.Tests --no-build
