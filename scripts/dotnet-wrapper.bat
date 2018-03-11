a4d517cf1ffa9405016e04662fc0db3ad7427f0a
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-11 14:09:30 +0100

    Move dotnet executable selection.

--- scripts/dotnet-wrapper.bat
+++ scripts/dotnet-wrapper.bat
@@ -3,12 +3,6 @@
 :: Extract all but the first of the arguments
 for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
 
-:: Determine the dotnet runtime to use
-set "DOTNET_EXE=C:\Program Files\dotnet\dotnet.exe"
-if "%PLATFORM%"=="x86" (
-	set "DOTNET_EXE=C:\Program Files (x86)\dotnet\dotnet.exe"
-)
-
 :: Cache the current platform variable, and set it to the passed-in variable
 set "INTERNAL_CACHED_PLATFORM=%PLATFORM%"
 set "PLATFORM=%1"
@@ -17,6 +11,12 @@ set "PLATFORM=%1"
 if "%PLATFORM"=="Any CPU" (
 	set "CACHED_PLATFORM=%PLATFORM%"
 	set PLATFORM=
+)
+
+:: Determine the dotnet runtime to use
+set "DOTNET_EXE=C:\Program Files\dotnet\dotnet.exe"
+if "%PLATFORM%"=="x86" (
+	set "DOTNET_EXE=C:\Program Files (x86)\dotnet\dotnet.exe"
 )
 
 "%DOTNET_EXE%" %ALL_BUT_FIRST%
