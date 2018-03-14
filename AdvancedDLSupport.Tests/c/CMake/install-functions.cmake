efc6ff76b878eff7373d1ec1e3f3f6a785f1598b
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-14 18:17:42 +0100

    Correct base output name retrieval.

--- AdvancedDLSupport.Tests/c/CMake/install-functions.cmake
+++ AdvancedDLSupport.Tests/c/CMake/install-functions.cmake
@@ -2,34 +2,34 @@ function(install_for_frameworks FRAMEWORKS)
     foreach(framework ${FRAMEWORKS})
         message("Framework: ${framework}")
         install(TARGETS
-                BaseTests
-                DisposeTests
-                EventTests
-                FunctionTests
-                LazyLoadingTests
-                PropertyTests
-                RemappingTests
-                TypeLoweringTests
-                AttributePassthroughTests
-                MixedModeTests
-                NullableTests
+                    BaseTests
+                    DisposeTests
+                    EventTests
+                    FunctionTests
+                    LazyLoadingTests
+                    PropertyTests
+                    RemappingTests
+                    TypeLoweringTests
+                    AttributePassthroughTests
+                    MixedModeTests
+                    NullableTests
                 COMPONENT
-                standard
+                    standard
                 DESTINATION
-                ${INSTALL_PATH_ABSOLUTE}/${framework})
+                    ${INSTALL_PATH_ABSOLUTE}/${framework})
 
         install(TARGETS
-                BaseTests-x64
+                    BaseTests-x64
                 COMPONENT
-                x64-only
+                    x64-only
                 DESTINATION
-                ${INSTALL_PATH_ABSOLUTE}/${framework}/lib/x64)
+                    ${INSTALL_PATH_ABSOLUTE}/${framework}/lib/x64)
 
         install(TARGETS
-                BaseTests-x32
+                    BaseTests-x32
                 COMPONENT
-                x32-only
+                    x32-only
                 DESTINATION
-                ${INSTALL_PATH_ABSOLUTE}/${framework}/lib/x32)
+                    ${INSTALL_PATH_ABSOLUTE}/${framework}/lib/x32)
     endforeach()
 endfunction()
