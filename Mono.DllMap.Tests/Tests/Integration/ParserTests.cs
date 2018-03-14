52807901930a7c2a796cd4c2cf97c12021058007
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-14 17:20:38 +0100

    Expand Mono.DllMap tests, and multitarget the test projects.

--- Mono.DllMap.Tests/Tests/Integration/ParserTests.cs
+++ Mono.DllMap.Tests/Tests/Integration/ParserTests.cs
@@ -8,133 +8,251 @@ namespace Mono.DllMap.Tests.Integration
 {
     public class ParserTests
     {
-        [Fact]
-        void CanParseSimpleConfig()
+        public class Parse
         {
-            using (var fs = File.OpenText(Path.Combine("Data", "DllMap.config.xml")))
+            [Fact]
+            public void CanParseSimpleConfig()
             {
-                var actual = DllConfiguration.Parse(fs);
+                using (var fs = File.OpenText(Path.Combine("Data", "DllMap.config.xml")))
+                {
+                    var actual = DllConfiguration.Parse(fs);
 
-                Assert.NotNull(actual.Maps);
-                Assert.Single(actual.Maps);
-                var map = actual.Maps.First();
+                    Assert.NotNull(actual.Maps);
+                    Assert.Single(actual.Maps);
+                    var map = actual.Maps.First();
 
-                Assert.Equal("cygwin1.dll", map.SourceLibrary);
-                Assert.Equal("libc.so.6", map.TargetLibrary);
+                    Assert.Equal("cygwin1.dll", map.SourceLibrary);
+                    Assert.Equal("libc.so.6", map.TargetLibrary);
 
-                Assert.NotNull(map.SymbolEntries);
-                Assert.Empty(map.SymbolEntries);
+                    Assert.NotNull(map.SymbolEntries);
+                    Assert.Empty(map.SymbolEntries);
 
-                Assert.True(map.Architecture.HasAll());
-                Assert.True(map.WordSize.HasAll());
-                Assert.True(map.OperatingSystems.HasAll());
+                    Assert.True(map.Architecture.HasAll());
+                    Assert.True(map.WordSize.HasAll());
+                    Assert.True(map.OperatingSystems.HasAll());
+                }
+            }
+
+            [Fact]
+            public void CanParseXmlString()
+            {
+                var text = File.ReadAllText(Path.Combine("Data", "DllMap.config.xml"));
+                var _ = DllConfiguration.Parse(text);
+            }
+
+            [Fact]
+            public void CanParseXmlStream()
+            {
+                using (var fs = File.OpenRead(Path.Combine("Data", "DllMap.config.xml")))
+                {
+                    var _ = DllConfiguration.Parse(fs);
+                }
+            }
+
+            [Fact]
+            public void CanParseXmlTextReader()
+            {
+                using (var fs = File.OpenText(Path.Combine("Data", "DllMap.config.xml")))
+                {
+                    var _ = DllConfiguration.Parse(fs);
+                }
+            }
+
+            [Fact]
+            public void CanParseDllEntryConfigThatMapsToMultipleLibraries()
+            {
+                using (var fs = File.OpenText(Path.Combine("Data", "DllEntry_MultiLib.config.xml")))
+                {
+                    var actual = DllConfiguration.Parse(fs);
+
+                    Assert.NotNull(actual.Maps);
+                    Assert.Single(actual.Maps);
+                    var map = actual.Maps.First();
+
+                    Assert.Equal("SolarSystem", map.SourceLibrary);
+                    Assert.Null(map.TargetLibrary);
+                    Assert.True(map.Architecture.HasAll());
+                    Assert.True(map.WordSize.HasAll());
+
+                    Assert.False(map.OperatingSystems.HasAll());
+                    Assert.False(map.OperatingSystems.HasFlagFast(Windows));
+
+                    Assert.NotNull(map.SymbolEntries);
+                    Assert.Equal(2, map.SymbolEntries.Count);
+                    var entry1 = map.SymbolEntries[0];
+                    var entry2 = map.SymbolEntries[1];
+
+                    Assert.Equal("libearth.so", entry1.TargetLibrary);
+                    Assert.Equal("libmars.so", entry2.TargetLibrary);
+
+                    Assert.Equal("get_Animals", entry1.SourceSymbol);
+                    Assert.Equal("get_Plants", entry2.SourceSymbol);
+
+                    Assert.True(map.Architecture.HasAll());
+                    Assert.True(map.WordSize.HasAll());
+
+                    Assert.False(entry1.OperatingSystems.HasAll());
+                    Assert.False(entry2.OperatingSystems.HasAll());
+
+                    Assert.False(entry1.OperatingSystems.HasFlagFast(Windows));
+                    Assert.False(entry2.OperatingSystems.HasFlagFast(Windows));
+                }
+            }
+
+            [Fact]
+            public void CanParseDllEntryConfigThatMapsForMultipleOperatingSystems()
+            {
+                using (var fs = File.OpenText(Path.Combine("Data", "DllEntry_MultiOS.config.xml")))
+                {
+                    var actual = DllConfiguration.Parse(fs);
+
+                    Assert.NotNull(actual.Maps);
+                    Assert.Single(actual.Maps);
+                    var map = actual.Maps.First();
+
+                    Assert.Equal("libc", map.SourceLibrary);
+                    Assert.Null(map.TargetLibrary);
+                    Assert.True(map.Architecture.HasAll());
+                    Assert.True(map.WordSize.HasAll());
+                    Assert.True(map.OperatingSystems.HasAll());
+
+                    Assert.NotNull(map.SymbolEntries);
+                    Assert.Equal(2, map.SymbolEntries.Count);
+                    var entry1 = map.SymbolEntries[0];
+                    var entry2 = map.SymbolEntries[1];
+
+                    Assert.True(entry1.OperatingSystems.HasAll());
+                    Assert.True(entry1.Architecture.HasAll());
+                    Assert.True(entry1.WordSize.HasAll());
+
+                    Assert.Equal("libdifferent.so", entry1.TargetLibrary);
+                    Assert.Equal("somefunction", entry1.SourceSymbol);
+                    Assert.Equal("differentfunction", entry1.TargetSymbol);
+
+                    Assert.False(entry2.OperatingSystems.HasAll());
+                    Assert.True(entry2.OperatingSystems.HasFlagsFast(Solaris, FreeBSD));
+                    Assert.True(entry2.Architecture.HasAll());
+                    Assert.True(entry2.WordSize.HasAll());
+
+                    Assert.Equal("libanother.so", entry2.TargetLibrary);
+                    Assert.Equal("somefunction", entry2.SourceSymbol);
+                    Assert.Equal("differentfunction", entry2.TargetSymbol);
+                }
             }
         }
 
-        [Fact]
-        void CanParseXmlString()
+        public class TryParse
         {
-            var text = File.ReadAllText(Path.Combine("Data", "DllMap.config.xml"));
-            var _ = DllConfiguration.Parse(text);
-        }
-
-        [Fact]
-        void CanParseXmlStream()
-        {
-            using (var fs = File.OpenRead(Path.Combine("Data", "DllMap.config.xml")))
+            [Fact]
+            public void ReturnsTrueForValidXmlString()
             {
-                var _ = DllConfiguration.Parse(fs);
+                var text = File.ReadAllText(Path.Combine("Data", "DllMap.config.xml"));
+                Assert.True(DllConfiguration.TryParse(text, out var _));
             }
-        }
 
-        [Fact]
-        void CanParseXmlTextReader()
-        {
-            using (var fs = File.OpenText(Path.Combine("Data", "DllMap.config.xml")))
+            [Fact]
+            public void ReturnsTrueForValidXmlStream()
             {
-                var _ = DllConfiguration.Parse(fs);
+                using (var fs = File.OpenRead(Path.Combine("Data", "DllMap.config.xml")))
+                {
+                    Assert.True(DllConfiguration.TryParse(fs, out var _));
+                }
             }
-        }
 
-        [Fact]
-        void CanParseDllEntryConfigThatMapsToMultipleLibraries()
-        {
-            using (var fs = File.OpenText(Path.Combine("Data", "DllEntry_MultiLib.config.xml")))
+            [Fact]
+            public void ReturnsTrueForValidXmlTextReader()
             {
-                var actual = DllConfiguration.Parse(fs);
-
-                Assert.NotNull(actual.Maps);
-                Assert.Single(actual.Maps);
-                var map = actual.Maps.First();
-
-                Assert.Equal("SolarSystem", map.SourceLibrary);
-                Assert.Null(map.TargetLibrary);
-                Assert.True(map.Architecture.HasAll());
-                Assert.True(map.WordSize.HasAll());
-
-                Assert.False(map.OperatingSystems.HasAll());
-                Assert.False(map.OperatingSystems.HasFlagFast(Windows));
-
-                Assert.NotNull(map.SymbolEntries);
-                Assert.Equal(2, map.SymbolEntries.Count);
-                var entry1 = map.SymbolEntries[0];
-                var entry2 = map.SymbolEntries[1];
-
-                Assert.Equal("libearth.so", entry1.TargetLibrary);
-                Assert.Equal("libmars.so", entry2.TargetLibrary);
-
-                Assert.Equal("get_Animals", entry1.SourceSymbol);
-                Assert.Equal("get_Plants", entry2.SourceSymbol);
-
-                Assert.True(map.Architecture.HasAll());
-                Assert.True(map.WordSize.HasAll());
-
-                Assert.False(entry1.OperatingSystems.HasAll());
-                Assert.False(entry2.OperatingSystems.HasAll());
-
-                Assert.False(entry1.OperatingSystems.HasFlagFast(Windows));
-                Assert.False(entry2.OperatingSystems.HasFlagFast(Windows));
+                using (var sr = File.OpenText(Path.Combine("Data", "DllMap.config.xml")))
+                {
+                    Assert.True(DllConfiguration.TryParse(sr, out var _));
+                }
             }
-        }
 
-        [Fact]
-        void CanParseDllEntryConfigThatMapsForMultipleOperatingSystems()
-        {
-            using (var fs = File.OpenText(Path.Combine("Data", "DllEntry_MultiOS.config.xml")))
+            [Fact]
+            public void OutVarIsNotNullForValidXmlString()
             {
-                var actual = DllConfiguration.Parse(fs);
+                var text = File.ReadAllText(Path.Combine("Data", "DllMap.config.xml"));
+                DllConfiguration.TryParse(text, out var val);
 
-                Assert.NotNull(actual.Maps);
-                Assert.Single(actual.Maps);
-                var map = actual.Maps.First();
+                Assert.NotNull(val);
+            }
 
-                Assert.Equal("libc", map.SourceLibrary);
-                Assert.Null(map.TargetLibrary);
-                Assert.True(map.Architecture.HasAll());
-                Assert.True(map.WordSize.HasAll());
-                Assert.True(map.OperatingSystems.HasAll());
+            [Fact]
+            public void OutVarIsNotNullForValidXmlStream()
+            {
+                using (var fs = File.OpenRead(Path.Combine("Data", "DllMap.config.xml")))
+                {
+                    DllConfiguration.TryParse(fs, out var val);
 
-                Assert.NotNull(map.SymbolEntries);
-                Assert.Equal(2, map.SymbolEntries.Count);
-                var entry1 = map.SymbolEntries[0];
-                var entry2 = map.SymbolEntries[1];
+                    Assert.NotNull(val);
+                }
+            }
 
-                Assert.True(entry1.OperatingSystems.HasAll());
-                Assert.True(entry1.Architecture.HasAll());
-                Assert.True(entry1.WordSize.HasAll());
+            [Fact]
+            public void OutVarIsNotNullForValidXmlTextReader()
+            {
+                using (var sr = File.OpenText(Path.Combine("Data", "DllMap.config.xml")))
+                {
+                    DllConfiguration.TryParse(sr, out var val);
 
-                Assert.Equal("libdifferent.so", entry1.TargetLibrary);
-                Assert.Equal("somefunction", entry1.SourceSymbol);
-                Assert.Equal("differentfunction", entry1.TargetSymbol);
+                    Assert.NotNull(val);
+                }
+            }
 
-                Assert.False(entry2.OperatingSystems.HasAll());
-                Assert.True(entry2.OperatingSystems.HasFlagsFast(Solaris, FreeBSD));
-                Assert.True(entry2.Architecture.HasAll());
-                Assert.True(entry2.WordSize.HasAll());
+            [Fact]
+            public void ReturnsFalseForInvalidXmlString()
+            {
+                var text = File.ReadAllText(Path.Combine("Data", "DllMap-bad.config.xml"));
+                Assert.False(DllConfiguration.TryParse(text, out var _));
+            }
 
-                Assert.Equal("libanother.so", entry2.TargetLibrary);
-                Assert.Equal("somefunction", entry2.SourceSymbol);
-                Assert.Equal("differentfunction", entry2.TargetSymbol);
+            [Fact]
+            public void ReturnsFalseForInvalidXmlStream()
+            {
+                using (var fs = File.OpenRead(Path.Combine("Data", "DllMap-bad.config.xml")))
+                {
+                    Assert.False(DllConfiguration.TryParse(fs, out var _));
+                }
+            }
+
+            [Fact]
+            public void ReturnsFalseForInvalidXmlTextReader()
+            {
+                using (var sr = File.OpenText(Path.Combine("Data", "DllMap-bad.config.xml")))
+                {
+                    Assert.False(DllConfiguration.TryParse(sr, out var _));
+                }
+            }
+
+            [Fact]
+            public void OutVarIsNullForInvalidXmlString()
+            {
+                var text = File.ReadAllText(Path.Combine("Data", "DllMap-bad.config.xml"));
+                DllConfiguration.TryParse(text, out var val);
+
+                Assert.Null(val);
+            }
+
+            [Fact]
+            public void OutVarIsNullForInvalidXmlStream()
+            {
+                using (var fs = File.OpenRead(Path.Combine("Data", "DllMap-bad.config.xml")))
+                {
+                    DllConfiguration.TryParse(fs, out var val);
+
+                    Assert.Null(val);
+                }
+            }
+
+            [Fact]
+            public void OutVarIsNullForInvalidXmlTextReader()
+            {
+                using (var sr = File.OpenText(Path.Combine("Data", "DllMap-bad.config.xml")))
+                {
+                    DllConfiguration.TryParse(sr, out var val);
+
+                    Assert.Null(val);
+                }
             }
         }
     }
