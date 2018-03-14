516e6aaa2c401c5d52c9b418e809faac2aadd72e
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-14 18:33:14 +0100

    Fix instrumentation paths.

--- scripts/travis-coverage.sh
+++ scripts/travis-coverage.sh
@@ -19,16 +19,27 @@ nuget install altcover -OutputDirectory altcover -Version 2.0.324
 
 # Instrument the test assemblies
 dotnet run --project altcover/altcover.2.0.324/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${Configuration} --\
- -i=AdvancedDLSupport.Tests/bin/${OutputDir}/netcoreapp2.0 -o=instrumented-adl -x=coverage-adl.xml --opencover\
+ -i=AdvancedDLSupport.Tests/bin/${OutputDir}/netcoreapp2.0 -o=instrumented-adl-netcoreapp2.0 -x=coverage-adl.xml --opencover\
  --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+ --assemblyExcludeFilter=Mono\.DllMap.+
 
 dotnet run --project altcover/altcover.2.0.324/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${Configuration} --\
- -i=Mono.DllMap.Tests/bin/${OutputDir}/netcoreapp2.0 -o=instrumented-mdl -x=coverage-mdl.xml --opencover\
+ -i=Mono.DllMap.Tests/bin/${OutputDir}/netcoreapp2.0 -o=instrumented-mdl-netcoreapp2.0 -x=coverage-mdl.xml --opencover\
+ --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+
+
+dotnet run --project altcover/altcover.2.0.324/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${Configuration} --\
+ -i=AdvancedDLSupport.Tests/bin/${OutputDir}/net461 -o=instrumented-adl-net461 -x=coverage-adl.xml --opencover\
+ --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+ --assemblyExcludeFilter=Mono\.DllMap.+
+
+dotnet run --project altcover/altcover.2.0.324/tools/netcoreapp2.0/AltCover/altcover.core.fsproj --configuration ${Configuration} --\
+ -i=Mono.DllMap.Tests/bin/${OutputDir}/net461 -o=instrumented-mdl-net461 -x=coverage-mdl.xml --opencover\
  --assemblyExcludeFilter=.+\.Tests --assemblyExcludeFilter=AltCover.+
 
 # Copy them to their build directories
-cp instrumented-adl/* AdvancedDLSupport.Tests/bin/${OutputDir}/netcoreapp2.0
-cp instrumented-mdl/* Mono.DllMap.Tests/bin/${OutputDir}/netcoreapp2.0
+cp instrumented-adl-netcoreapp2.0/* AdvancedDLSupport.Tests/bin/${OutputDir}/netcoreapp2.0
+cp instrumented-mdl-netcoreapp2.0/* Mono.DllMap.Tests/bin/${OutputDir}/netcoreapp2.0
+
+cp instrumented-adl-net461/* AdvancedDLSupport.Tests/bin/${OutputDir}/net461
+cp instrumented-mdl-net461/* Mono.DllMap.Tests/bin/${OutputDir}/net461
 
 # And run coverage
 cd AdvancedDLSupport.Tests
