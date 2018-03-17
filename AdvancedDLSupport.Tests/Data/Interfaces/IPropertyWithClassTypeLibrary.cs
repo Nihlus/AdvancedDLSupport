479e6af9f945554d42fed8ba4cafe6bd06417cd5
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-03-17 13:27:58 +0100

    Sort property tests, and implement failure case test.

---
+++ AdvancedDLSupport.Tests/Data/Interfaces/IPropertyWithClassTypeLibrary.cs
@@ -0,0 +1,9 @@
+﻿using AdvancedDLSupport.Tests.Data.Classes;
+
+namespace AdvancedDLSupport.Tests.Data
+{
+    public interface IPropertyWithClassTypeLibrary
+    {
+        SimpleClass MyClass { get; set; }
+    }
+}
--- AdvancedDLSupport.Tests/Tests/Integration/PropertyIntegrationTests.cs
+++ AdvancedDLSupport.Tests/Tests/Integration/PropertyIntegrationTests.cs
@@ -8,80 +8,91 @@ using Xunit;
 
 namespace AdvancedDLSupport.Tests.Integration
 {
-	public class PropertyIntegrationTests : LibraryTestBase<IPropertyLibrary>
+	public class PropertyIntegrationTests
 	{
 		private const string LibraryName = "PropertyTests";
 
-		public PropertyIntegrationTests() : base(LibraryName)
+		public class Getter : LibraryTestBase<IPropertyLibrary>
 		{
+			public Getter() : base(LibraryName)
+			{
+				Library.ResetData();
+			}
+
+			[Fact]
+			public void CanGetGlobalVariableAsProperty()
+			{
+				Assert.Equal(5, Library.GlobalVariable);
+			}
+
+			[Fact]
+			public void CanGetGlobalVariableAsGetOnlyProperty()
+			{
+				Assert.Equal(5, Library.GlobalVariableGetOnly);
+			}
+
+			[Fact]
+			public unsafe void CanGetGlobalPointerVariableAsProperty()
+			{
+				Assert.Equal(20, *Library.GlobalPointerVariable);
+			}
+
+			[Fact]
+			public unsafe void CanGetGlobalPointerVariableAsGetOnlyProperty()
+			{
+				Assert.Equal(20, *Library.GlobalPointerVariableGetOnly);
+			}
 		}
 
-		[Fact]
-		public void CanGetGlobalVariableAsProperty()
+		public class Setter : LibraryTestBase<IPropertyLibrary>
 		{
-			Library.ResetData();
+			public Setter() : base(LibraryName)
+			{
+				Library.ResetData();
+			}
 
-			Assert.Equal(5, Library.GlobalVariable);
+			[Fact]
+			public void CanSetGlobalVariableAsProperty()
+			{
+				Library.GlobalVariable = 1;
+				Assert.Equal(1, Library.GlobalVariable);
+			}
+
+			[Fact]
+			public void CanSetGlobalVariableAsSetOnlyProperty()
+			{
+				Library.GlobalVariableSetOnly = 1;
+				Assert.Equal(1, Library.GlobalVariable);
+			}
+
+			[Fact]
+			public unsafe void CanSetGlobalPointerVariableAsProperty()
+			{
+				*Library.GlobalPointerVariable = 25;
+				Assert.Equal(25, *Library.GlobalPointerVariable);
+			}
+
+			[Fact]
+			public unsafe void CanSetGlobalPointerVariableAsSetOnlyProperty()
+			{
+				Marshal.StructureToPtr(25, new IntPtr(Library.GlobalPointerVariableGetOnly), false);
+				Assert.Equal(25, *Library.GlobalPointerVariable);
+			}
 		}
 
-		[Fact]
-		public void CanSetGlobalVariableAsProperty()
+		public class FailureCases
 		{
-			Library.ResetData();
+			[Fact]
+			public void ThrowsNotSupportedExceptionIfPropertyHasClassType()
+			{
+				var builder = NativeLibraryBuilder.Default;
 
-			Library.GlobalVariable = 1;
-			Assert.Equal(1, Library.GlobalVariable);
-		}
-
-		[Fact]
-		public void CanGetGlobalVariableAsGetOnlyProperty()
-		{
-			Library.ResetData();
-
-			Assert.Equal(5, Library.GlobalVariableGetOnly);
-		}
-
-		[Fact]
-		public void CanSetGlobalVariableAsSetOnlyProperty()
-		{
-			Library.ResetData();
-
-			Library.GlobalVariableSetOnly = 1;
-			Assert.Equal(1, Library.GlobalVariable);
-		}
-
-		[Fact]
-		public unsafe void CanGetGlobalPointerVariableAsProperty()
-		{
-			Library.ResetData();
-
-			Assert.Equal(20, *Library.GlobalPointerVariable);
-		}
-
-		[Fact]
-		public unsafe void CanSetGlobalPointerVariableAsProperty()
-		{
-			Library.ResetData();
-
-			*Library.GlobalPointerVariable = 25;
-			Assert.Equal(25, *Library.GlobalPointerVariable);
-		}
-
-		[Fact]
-		public unsafe void CanGetGlobalPointerVariableAsGetOnlyProperty()
-		{
-			Library.ResetData();
-
-			Assert.Equal(20, *Library.GlobalPointerVariableGetOnly);
-		}
-
-		[Fact]
-		public unsafe void CanSetGlobalPointerVariableAsSetOnlyProperty()
-		{
-			Library.ResetData();
-
-			Marshal.StructureToPtr(25, new IntPtr(Library.GlobalPointerVariableGetOnly), false);
-			Assert.Equal(25, *Library.GlobalPointerVariable);
+				Assert.Throws<NotSupportedException>
+				(
+					() =>
+						builder.ActivateInterface<IPropertyWithClassTypeLibrary>(LibraryName)
+				);
+			}
 		}
 	}
 }
