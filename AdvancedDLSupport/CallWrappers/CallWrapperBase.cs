eabceb2bf2e1448fcf64e8ff2d29fa7c8e894bb7
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-04-20 21:39:02 +0200

    Add call wrapper base class.

---
+++ AdvancedDLSupport/CallWrappers/CallWrapperBase.cs
@@ -0,0 +1,114 @@
+﻿//
+//  CallWrapperBase.cs
+//
+//  Copyright (c) 2018 Firwood Software
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using AdvancedDLSupport.Extensions;
+using AdvancedDLSupport.ImplementationGenerators;
+using AdvancedDLSupport.Pipeline;
+using AdvancedDLSupport.Reflection;
+using JetBrains.Annotations;
+using StrictEmit;
+
+namespace AdvancedDLSupport.CallWrappers
+{
+    /// <summary>
+    /// Base class for call wrappers. This class implements some base functionality which allows inheritors to abstain
+    /// from emitting prologues or epilogues.
+    /// </summary>
+    public abstract class CallWrapperBase : ImplementationGeneratorBase<IntrospectiveMethodInfo>, ICallWrapper
+    {
+        /// <inheritdoc />
+        public abstract ImplementationOptions Options { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CallWrapperBase"/> class.
+        /// </summary>
+        /// <param name="targetModule">The module where the implementation should be generated.</param>
+        /// <param name="targetType">The type in which the implementation should be generated.</param>
+        /// <param name="targetTypeConstructorIL">The IL generator for the target type's constructor.</param>
+        /// <param name="options">The configuration object to use.</param>
+        protected CallWrapperBase
+        (
+            [NotNull] ModuleBuilder targetModule,
+            [NotNull] TypeBuilder targetType,
+            [NotNull] ILGenerator targetTypeConstructorIL,
+            ImplementationOptions options
+        )
+            : base(targetModule, targetType, targetTypeConstructorIL, options)
+        {
+        }
+
+        /// <inheritdoc />
+        public sealed override IEnumerable<PipelineWorkUnit<IntrospectiveMethodInfo>> GenerateImplementation
+        (
+            PipelineWorkUnit<IntrospectiveMethodInfo> workUnit
+        )
+        {
+            var definition = workUnit.Definition;
+
+            if (!(definition.GetWrappedMember() is MethodBuilder builder))
+            {
+                throw new ArgumentNullException(nameof(workUnit), "Could not unwrap introspective method to method builder.");
+            }
+
+            var passthroughMethod = TargetType.DefineMethod
+            (
+                $"{workUnit.GetUniqueBaseMemberName()}_wrapper",
+                workUnit.Definition.Attributes,
+                CallingConventions.Standard,
+                workUnit.Definition.ReturnType,
+                workUnit.Definition.ParameterTypes.ToArray()
+            );
+
+            passthroughMethod.ApplyCustomAttributesFrom(definition);
+
+            var il = builder.GetILGenerator();
+
+            EmitPrologue(il, workUnit);
+            il.EmitCallDirect(passthroughMethod);
+            EmitEpilogue(il, workUnit);
+            il.EmitReturn();
+
+            // Pass through the method
+            var newMethodInfo = new IntrospectiveMethodInfo(passthroughMethod);
+            yield return new PipelineWorkUnit<IntrospectiveMethodInfo>(newMethodInfo, workUnit);
+        }
+
+        /// <inheritdoc />
+        public abstract bool IsApplicable(IntrospectiveMethodInfo method, ImplementationOptions options);
+
+        /// <inheritdoc />
+        public virtual void EmitPrologue(ILGenerator il, PipelineWorkUnit<IntrospectiveMethodInfo> workUnit)
+        {
+            for (short i = 1; i <= workUnit.Definition.ParameterTypes.Count; ++i)
+            {
+                il.EmitLoadArgument(i);
+            }
+        }
+
+        /// <inheritdoc />
+        public virtual void EmitEpilogue(ILGenerator il, PipelineWorkUnit<IntrospectiveMethodInfo> workUnit)
+        {
+        }
+    }
+}
---
+++ AdvancedDLSupport/CallWrappers/ICallWrapper.cs
@@ -0,0 +1,59 @@
+﻿//
+//  ICallWrapper.cs
+//
+//  Copyright (c) 2018 Firwood Software
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using System.Reflection.Emit;
+using AdvancedDLSupport.ImplementationGenerators;
+using AdvancedDLSupport.Pipeline;
+using AdvancedDLSupport.Reflection;
+using JetBrains.Annotations;
+
+namespace AdvancedDLSupport.CallWrappers
+{
+    /// <summary>
+    /// Represents a wrapper emitter that accepts a method, emits arbitrary prologue instructions, calls the method, and
+    /// then emits arbitrary epilogue instructions. Typically, the input method is passed through without modifications.
+    /// </summary>
+    public interface ICallWrapper : IImplementationGenerator<IntrospectiveMethodInfo>
+    {
+        /// <summary>
+        /// Determines whether or not the call wrapper is applicable for the given method definition.
+        /// </summary>
+        /// <param name="method">The method definition.</param>
+        /// <param name="options">The relevant implementation options.</param>
+        /// <returns>true if the wrapper is applicable; otherwise, false.</returns>
+        bool IsApplicable([NotNull] IntrospectiveMethodInfo method, ImplementationOptions options);
+
+        /// <summary>
+        /// Emits the wrapper prologue, that is, the instructions before the method call. The evaluation stack is clean
+        /// at the start of this method. Immediately following this method, a call to the wrapped method will be made.
+        /// </summary>
+        /// <param name="il">The generator where the instructions will be emitted.</param>
+        /// <param name="workUnit">The method being worked on.</param>
+        void EmitPrologue([NotNull] ILGenerator il, [NotNull] PipelineWorkUnit<IntrospectiveMethodInfo> workUnit);
+
+        /// <summary>
+        /// Emits the wrapper prologue, that is, the instructions after the method call. The return value (if any) of
+        /// the wrapped method will be on top of the evaluation stack at the beginning of this method. The value on top
+        /// of the evaluation stack at the end of the epilogue (if any) will be returned to the caller.
+        /// </summary>
+        /// <param name="il">The generator where the instructions will be emitted.</param>
+        /// <param name="workUnit">The method being worked on.</param>
+        void EmitEpilogue([NotNull] ILGenerator il, [NotNull] PipelineWorkUnit<IntrospectiveMethodInfo> workUnit);
+    }
+}
