85d98418aa9ec2fb42e390c9a12b64d803bce703
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-09-14 21:18:23 +0200

    Use much tighter integrated altcover.

--- scripts/appveyor-coverage.ps1
+++ scripts/appveyor-coverage.ps1
@@ -1,66 +1,23 @@
 $DOTNET = "C:\Program Files\dotnet\dotnet.exe"
-$ASSEMBLY_OUTPUT_DIR = "$env:CONFIGURATION"
 
-$ALTCOVER_VERSION = "3.0.422"
-$ALTCOVER_PATH = "altcover\altcover.$ALTCOVER_VERSION\tools\netcoreapp2.0\AltCover.dll"
-
-$RUNTIME_VERSION = (dotnet --info | Select-String -Pattern "Host" -Context 0, 1).Context.PostContext[0] | % { [regex]::Match($_, "(\d\.?)+").Value }
-
-$EXTRA_ARGS = ""
-if ($env:PLATFORM -eq "x86" -Or $env:PLATFORM -eq "x64")
+if ($env:PLATFORM -eq "x86")
 {
-    if ($env:PLATFORM -eq "x86")
-    {
-        $DOTNET = "C:\Program Files (x86)\dotnet\dotnet.exe"
-        $EXTRA_ARGS = "-x86"
-    }
-
-    $ASSEMBLY_OUTPUT_DIR = "$env:PLATFORM\$env:CONFIGURATION"
+    $DOTNET = "C:\Program Files (x86)\dotnet\dotnet.exe"
 }
 
-# Fix inconsistent spacing in platform targets
-if ($env:PLATFORM -eq "Any CPU")
+function Run-Coverage([string]$project)
 {
-    $env:PLATFORM = "AnyCPU"
-}
-
-function Run-Coverage([string]$project, [string]$framework)
-{
-    $INPUT_DIRECTORY = "$project\bin\$ASSEMBLY_OUTPUT_DIR\$framework"
-    $OUTPUT_DIRECTORY = "instrumented\$project-$framework"
-    $XMLREPORT = "coverage-$project-$framework.xml"
-
-    & $DOTNET $ALTCOVER_PATH `
-    --inputDirectory $INPUT_DIRECTORY `
-    --outputDirectory $OUTPUT_DIRECTORY `
-    --xmlReport $XMLREPORT `
-    --opencover `
-    --save `
-    --inplace `
-    --assemblyExcludeFilter ".+\.Tests" `
-    --assemblyExcludeFilter "AltCover.+"
+    $XMLREPORT = "coverage-$project.xml"
 
     Push-Location -Path $project
 
-    & $DOTNET xunit -nobuild -noshadow -framework $FRAMEWORK -configuration $env:CONFIGURATION $EXTRA_ARGS
+    & $DOTNET test /p:AltCover=true /p:CopyLocalLockFileAssemblies=true /p:AltCoverXmlReport=$XMLREPORT --configuration $env:CONFIGURATION
+
+    Move-Item coverage*.xml -Destination ..
 
     Pop-Location
-
-    & $DOTNET $ALTCOVER_PATH runner --recorderDirectory $INPUT_DIRECTORY --collect
 }
 
-nuget install altcover -OutputDirectory altcover -Version $ALTCOVER_VERSION
-
-Run-Coverage "AdvancedDLSupport.Tests" "netcoreapp2.0"
-Run-Coverage "AdvancedDLSupport.Tests" "net461"
-
-Run-Coverage "AdvancedDLSupport.AOT.Tests" "net461"
-
-Run-Coverage "Mono.DllMap.Tests" "netcoreapp2.0"
-Run-Coverage "Mono.DllMap.Tests" "net461"
-
-# Restore inconsistent spacing in platform targets
-if ($env:PLATFORM -eq "AnyCPU")
-{
-    $env:PLATFORM = "Any CPU"
-}
+Run-Coverage "AdvancedDLSupport.Tests"
+Run-Coverage "AdvancedDLSupport.AOT.Tests"
+Run-Coverage "Mono.DllMap.Tests"
--- scripts/travis-coverage.sh
+++ scripts/travis-coverage.sh
@@ -1,62 +1,21 @@
 #!/bin/bash
 
-OutputDir="${Configuration}"
-if [ "${Platform}" == "x86" ] || [ "${Platform}" == "x64" ]; then
-	OutputDir="${Platform}/${Configuration}"
-fi
-
-# Fix inconsistent spacing in platform targets
-if [ "${Platform}" == "Any CPU" ]; then
-	Platform="AnyCPU"
-fi
-
-ALTCOVER_VERSION="3.0.422"
-ALTCOVER_PATH="altcover/altcover.$ALTCOVER_VERSION/tools/netcoreapp2.0/AltCover.dll"
-
-RUNTIME_VERSION=$(dotnet --info | sed -n '/Microsoft .NET Core Shared Framework Host/,$p' | grep Version | awk '{print $3}')
-
 function runCoverage
 {
 	PROJECT=$1
-	FRAMEWORK=$2
 
-	INPUT_DIRECTORY="$PROJECT/bin/$OutputDir/$FRAMEWORK"
-	OUTPUT_DIRECTORY="instrumented/$PROJECT-$FRAMEWORK"
-	XMLREPORT="coverage-$PROJECT-$FRAMEWORK.xml"
-
-	dotnet ${ALTCOVER_PATH} \
-	--inputDirectory ${INPUT_DIRECTORY} \
-	--outputDirectory ${OUTPUT_DIRECTORY} \
-	--xmlReport ${XMLREPORT} \
-	--opencover \
-	--save \
-	--inplace \
-	--assemblyExcludeFilter ".+\.Tests" \
-	--assemblyExcludeFilter "AltCover.+"
+	XMLREPORT="coverage-$PROJECT.xml"
 
 	cd ${PROJECT}
 
-	dotnet xunit -nobuild -noshadow -framework ${FRAMEWORK} -fxversion ${RUNTIME_VERSION} -configuration ${Configuration}
+	dotnet test /p:AltCover=true /p:CopyLocalLockFileAssemblies=true /p:AltCoverXmlReport=${XMLREPORT} --configuration ${Configuration}
+
+	mv coverage*.xml ../
 
 	cd -
-
-	dotnet ${ALTCOVER_PATH} runner --recorderDirectory ${INPUT_DIRECTORY} --collect
 }
 
-# Install AltCover
-nuget install altcover -OutputDirectory altcover -Version ${ALTCOVER_VERSION}
-
 # Run coverage
-runCoverage AdvancedDLSupport.Tests netcoreapp2.0
-runCoverage AdvancedDLSupport.Tests net461
-
-runCoverage AdvancedDLSupport.AOT.Tests net461
-
-runCoverage Mono.DllMap.Tests netcoreapp2.0
-runCoverage Mono.DllMap.Tests net461
-
-# Restore inconsistent spacing in platform targets
-if [ "${Platform}" == "AnyCPU" ]; then
-	Platform="Any CPU"
-fi
-
+runCoverage AdvancedDLSupport.Tests
+runCoverage AdvancedDLSupport.AOT.Tests
+runCoverage Mono.DllMap.Tests
