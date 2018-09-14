345edca3c32427876726752d3f12281c59d8fbbb
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-09-14 17:26:00 +0200

    Add some more tests for the other attribute permutation cases.

--- AdvancedDLSupport.Tests/Tests/Integration/MixedModeTests.cs
+++ AdvancedDLSupport.Tests/Tests/Integration/MixedModeTests.cs
@@ -64,6 +64,20 @@ namespace AdvancedDLSupport.Tests.Integration
         }
 
         [Fact]
+        public void CanActivateClassWithNativeSymbolInClass()
+        {
+            var library = _builder.ActivateClass<MixedModeClassWithNativeSymbolInClass>(LibraryName);
+            Assert.Equal(5, library.SubtractWithRemappedName(10, 5));
+        }
+
+        [Fact]
+        public void CanActivateClassWithNativeSymbolInBothInterfaceAndClass()
+        {
+            var library = _builder.ActivateClass<MixedModeClassWithNativeSymbolInInterfaceAndClass>(LibraryName);
+            Assert.Equal(5, library.SubtractWithRemappedName(10, 5));
+        }
+
+        [Fact]
         public void CanActivateClassWithInheritedNativeInterfaces()
         {
             _builder.ActivateClass<MixedModeClassWithInheritedInterface>(LibraryName);
