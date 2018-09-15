ce1e69e1cf0a68f56e962a9d088d4c6b9fdf5f54
Jarl Gullberg <jarl.gullberg@gmail.com>
2018-09-15 17:35:02 +0200

    Don't pass an empty Platform property if Platform isn't set.

--- scripts/travis-build.sh
+++ scripts/travis-build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-dotnet build --configuration "${Configuration}" /p:Platform="${Platform}"
+if [ -z "${Platform}" ]; then
+	dotnet build --configuration "${Configuration}"
+else
+	dotnet build --configuration "${Configuration}" /p:Platform="${Platform}"
+fi
+
+
