573143b03b7fea78c10516870e598c739218d8e0
Unknown <17joshuanewcomb@gmail.com>
2019-06-18 11:37:14 -0700

    Decouple Library and Symbol loading

---
+++ AdvancedDLSupport/Loaders/ILibraryLoader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace AdvancedDLSupport.Loaders
+{
+    /// <summary>
+    /// Represents a class which can load Libraries on a specific platform.
+    /// </summary>
+    public interface ILibraryLoader
+    {
+        /// <summary>
+        /// Load the given library. A null path signifies intent to load the main executable instead of an external
+        /// library.
+        /// </summary>
+        /// <param name="path">The path to the library.</param>
+        /// <returns>A handle to the library. This value carries no intrinsic meaning.</returns>
+        /// <exception cref="LibraryLoadingException">Thrown if the library could not be loaded.</exception>
+        [PublicAPI]
+        IntPtr LoadLibrary([CanBeNull] string path);
+
+        /// <summary>
+        /// Closes the open handle to the given library.
+        /// </summary>
+        /// <param name="library">The handle to the library to close.</param>
+        /// <returns>true if the library was closed successfully; otherwise, false.</returns>
+        [PublicAPI]
+        bool CloseLibrary(IntPtr library);
+    }
+}
---
+++ AdvancedDLSupport/Loaders/ISymbolLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace AdvancedDLSupport.Loaders
+{
+    /// <summary>
+    /// Represents a class which can load Symbols on a specific platform.
+    /// </summary>
+    public interface ISymbolLoader
+    {
+        /// <summary>
+        /// Load the given symbol.
+        /// </summary>
+        /// <param name="library">The handle to the library in which the symbol exists.</param>
+        /// <param name="symbolName">The name of the symbol to load.</param>
+        /// <exception cref="SymbolLoadingException">Thrown if the symbol could not be loaded.</exception>
+        /// <returns>A handle to the symbol.</returns>
+        [PublicAPI, Pure]
+        IntPtr LoadSymbol(IntPtr library, [NotNull] string symbolName);
+
+        /// <summary>
+        /// Loads the given symbol name and marshals it into a function delegate.
+        /// </summary>
+        /// <param name="library">The library handle.</param>
+        /// <param name="symbolName">The name of the symbol.</param>
+        /// <typeparam name="T">The delegate type to marshal.</typeparam>
+        /// <returns>A marshalled delegate.</returns>
+        [PublicAPI, NotNull, Pure]
+        T LoadFunction<T>(IntPtr library, [NotNull] string symbolName);
+    }
+}
