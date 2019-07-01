42a00f5ad64de51b42d644fb71748b9a238f7559
Unknown <17joshuanewcomb@gmail.com>
2019-07-01 11:22:42 -0700

    Basic Tests implemented

---
+++ AdvancedDLSupport.Tests/Tests/Integration/CallingConventionTests.cs
@@ -0,0 +1,50 @@
+﻿//
+//  CallingConventionTests.cs
+//
+//  Copyright (c) 2018 Firwood Software
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+using AdvancedDLSupport.Tests.Data.Interfaces;
+using AdvancedDLSupport.Tests.TestBases;
+
+using Xunit;
+
+#pragma warning disable SA1600, CS1591
+
+namespace AdvancedDLSupport.Tests.Integration
+{
+    public class CallingConventionTests : LibraryTestBase<ICallingConventionLibrary>
+    {
+        private const string LibraryName = "CallingConventionTests";
+
+        public CallingConventionTests()
+            : base(LibraryName)
+        {
+        }
+
+        [Fact]
+        public void StdCallIsDefaultInterfaceConvention()
+        {
+            Assert.Equal(20, Library.MultiplyBy5_STD(4));
+        }
+
+        [Fact]
+        public void NativeSymbolOverrideWorks()
+        {
+            Assert.Equal(20, Library.MultiplyBy5(4));
+        }
+    }
+}
---
+++ AdvancedDLSupport.Tests/c/src/CallingConventionTests.c
@@ -0,0 +1,11 @@
+﻿#include <stdint.h>
+
+__declspec(dllexport) int32_t __stdcall MultiplyBy5_STD(int32_t num)
+{
+	return num * 5;
+}
+
+__declspec(dllexport) int32_t __cdecl MultiplyBy5(int32_t num)
+{
+	return num * 5;
+}
