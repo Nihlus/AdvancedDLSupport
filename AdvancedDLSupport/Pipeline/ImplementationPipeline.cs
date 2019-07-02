5b0665836716865f7789f38937f757eccead5436
jvbsl <jvsbl@gmx.de>
2019-07-02 18:41:46 +0200

    Feature/mod reqopt modifiers (#88)
    * Added ability to create methods with Required and Optional modifier parameters and return values
    
    * Set higher language version for testing of in-parameters in rider
    
    * * Renamed properties to full names
    * Try language version 7.2(for travis)

--- AdvancedDLSupport/Pipeline/ImplementationPipeline.cs
+++ AdvancedDLSupport/Pipeline/ImplementationPipeline.cs
@@ -231,7 +231,11 @@ namespace AdvancedDLSupport.Pipeline
                 Public | Final | Virtual | HideBySig | NewSlot,
                 CallingConventions.Standard,
                 interfaceDefinition.ReturnType,
-                interfaceDefinition.ParameterTypes.ToArray()
+                interfaceDefinition.ReturnParameterRequiredModifiers,
+                interfaceDefinition.ReturnParameterOptionalModifiers,
+                interfaceDefinition.ParameterTypes.ToArray(),
+                interfaceDefinition.ParameterRequiredModifiers.ToArray(),
+                interfaceDefinition.ParameterOptionalModifiers.ToArray()
             );
 
             // In the following blocks, which set of attributes to pass through is selected. The logic is as follows:
