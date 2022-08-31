1d21b5d4dfc649f8e9280601bbd5a9c709c36aba
Jarl Gullberg <jarl.gullberg@gmail.com>
2022-08-31 17:36:33 +0200

    Remove Travis and AppVeyor CI configuration.

--- scripts/appveyor-install-dotnet-32.ps1
+++
@@ -1,5 +0,0 @@
-if ($env:PLATFORM -eq "x86")
-{
-    Invoke-WebRequest -Uri 'https://dot.net/v1/dotnet-install.ps1' -OutFile C:\dotnet-install.ps1
-    C:\dotnet-install.ps1 -Channel Current -InstallDir "C:\Program Files (x86)\dotnet" -Architecture x86 -NoPath
-}
--- scripts/travis-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z "${Platform}" ]; then
-	dotnet build --configuration "${Configuration}"
-else
-	dotnet build --configuration "${Configuration}" /p:Platform="${Platform}"
-fi
-
-
